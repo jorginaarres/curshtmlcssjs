d675f145269ac1ba977930b7b23efa18c6b05f6b
jorginaarres <36164106+jorginaarres@users.noreply.github.com>
2018-02-06 15:29:49 +0100

    mesos

---
+++ mesos.js
@@ -0,0 +1,7 @@
+var mesos = ["Gener", "Febrer", "Març","Abril", "Maig", "Juny","Juliol", "Agost", "Setembre","Octubre", "Novembre", "Desembre"];
+
+function escriuremesos() {
+	for(i=0;i<12;i++)
+	console.log(mesos[i]);
+   
+}

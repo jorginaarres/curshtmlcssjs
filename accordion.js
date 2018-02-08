d74335ff98a5fcf07f0c576eded85dc5b04e2e14
jorginaarres <36164106+jorginaarres@users.noreply.github.com>
2018-02-08 18:28:59 +0100

    jqueryui

---
+++ accordion.js
@@ -0,0 +1,13 @@
+$(window).on("load",function(){
+       $(".a").forEach(function(){
+          $(this).click(function(){
+                  alert();
+               $( "#accordion" ).accordion();
+          });
+
+    });
+
+});
+
+    
+
